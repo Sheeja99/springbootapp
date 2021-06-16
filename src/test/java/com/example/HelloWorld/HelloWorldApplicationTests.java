bf1d6468236088ec598a8b1b54414b637d40d772
Sheeja99 <sheejaphilip99@gmail.com>
2021-06-16 16:39:19 +0530

    first commit

---
+++ src/test/java/com/example/HelloWorld/HelloWorldApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example.HelloWorld;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootTest
+class HelloWorldApplicationTests 
+{
+	@Controller
+	@EnableAutoConfiguration
+	public class HelloWorldController 
+	{
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String sayHello() 
+	{
+	return "Hello World ";
+	}
+	}
+}
+
